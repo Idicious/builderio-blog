e42377d444efa12e2a368b2499bcf07f961ef6b3
Henk Bakker <henk.bakker02@essent.nl>
2023-12-24 13:05:03 +0100

    fix analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({
   Component,
@@ -9,6 +10,7 @@ export default function App({
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      <Analytics />
     </SessionProvider>
   );
 }
